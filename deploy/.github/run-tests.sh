4bc988c78577a2b8a91bb8c7fc33b52191119ac2
Andrey Donits <96722829+AndyDonits@users.noreply.github.com>
2022-12-13 16:18:21 +0200

    npx install cypress --force

--- deploy/.github/run-tests.sh
+++ deploy/.github/run-tests.sh
@@ -4,7 +4,7 @@ mv automation-sample/* .
 echo `ls`
 echo `ls automation-sample`
 rmdir automation-sample
-npm install cypress
+npx install cypress --force
 npx cypress run
 RESULTS_READY=`echo $?`
 

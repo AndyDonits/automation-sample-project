a4382881ad6b43b015570b588bd20b6efc87f9a1
Andrey Donits <96722829+AndyDonits@users.noreply.github.com>
2022-12-13 17:28:15 +0200

    mv automation-sample/cypress

--- deploy/.github/run-tests.sh
+++ deploy/.github/run-tests.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 mv automation-sample/cypress.config.js .
+mv automation-sample/cypress .
 echo `ls`
 npm install
 npx cypress install
 (npm run start&)
 npx cypress run
 RESULTS_READY=`echo $?`
+lsof -nti:3000 | xargs kill -9
 
 exit $RESULTS_READY
